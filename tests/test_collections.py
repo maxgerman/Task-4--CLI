eb884004ae0390180a3ad2259f69db8368d742b5
Maksym German <maxjerman@gmail.com>
2021-04-13 16:10:23 +0300

    Corrections: optimized 'unique_chars' func, added type annotations to functions, added examples to test docstrings, added docstrings to functions and the module

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -32,23 +32,40 @@ CLI_parser_params = [
 
 
 def test_unique_chars_typical():
+    """Test that 'unique_chars' function is working
+    Example:
+        'abc' -> 3
+        'aaabc' -> 3
+    """
     for inp_val, out_val in typical_param:
         assert unique_chars(inp_val) == out_val
 
 
 def test_unique_chars_atypical():
+    """Test that 'unique_chars' function raises TypeError if not a string is passed
+    Example:
+        123 -> raises TypeError
+    """
     with pytest.raises(TypeError):
         unique_chars(123)
 
 
 def test_lru_cache_hits():
-    '''check if cache is hitting'''
+    """Check if cache count is increasing when same string is passed to the 'unique_chars' func. more than once
+    Example:
+        'abc' -> cache_info count > 0
+    """
     assert unique_chars.cache_info()[0] > 0
 
 
 @pytest.mark.parametrize('test_input, expected', CLI_typical_str_params)
 def test_strings_from_CLI(test_input, expected, monkeypatch, capfd):
-    """tests that input is read form the CLI and the correct prints are made in response"""
+    """Tests that input is read form the CLI and the correct prints are made in response
+    sys.argv is monkeypatched to the test values
+    'capfd' is a pytest fixture used to intercept the prints
+    Example:
+        '-s abc' in sys.argv -> 3 printed
+    """
     test_argv = ['progname', *test_input]
     monkeypatch.setattr('sys.argv', test_argv)
     collections_framework.main()
@@ -58,13 +75,22 @@ def test_strings_from_CLI(test_input, expected, monkeypatch, capfd):
 
 @pytest.mark.parametrize('test_input, property, expected', CLI_parser_params)
 def test_parser(test_input, property, expected):
-    """tests that parser receives and stores CLI parameters in the appropriate properties"""
+    """Test that parser receives and stores CLI parameters in the appropriate properties
+    Example:
+        '-s abc' -> parser_namespace.string = 'abc'
+        '-f filename.txt' -> parser_namespace.file = 'filename.txt'
+        """
     test_ns, parser = collections_framework.parse_cli_args(test_input)
     assert getattr(test_ns, property)[0] == expected
 
 
 def test_parser_file(capfd):
-    """reading from a mock file, asserting correct prints"""
+    """Reading from a mock file, assert correct prints.
+    'capfd' is a pytest fixture used to intercept the prints
+    Example:
+        'example.txt' file content = 'this is an example text file'
+        -f example.txt -> 5
+    """
     mock_file_contents = 'this is an example text file'
     with mock.patch('collections_fw.collections_framework.open', mock.mock_open(read_data=mock_file_contents)) as m:
         collections_framework.main(['-f', 'example.txt'])
